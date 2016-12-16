5c6426be26b892b5861461c7c7c494b6cd63df5d
mxy2008 <xinyima9@gmail.com>
2016-12-15 22:25:24 -0500

    scripts

--- setupEC2.sh
+++ setupEC2.sh
@@ -1,6 +1,6 @@
-rm -r timeseriesDB 
-rm -r ts_db_index
-rm id.json
-rm -r ts_data
-rm API/app.db
-python3 setup_DB.py
+rm -r cs207ddl/timeseriesDB 
+rm -r cs207ddl/ts_db_index
+rm cs207ddl/id.json
+rm -r cs207ddl/ts_data
+rm cs207ddl/API/app.db
+python3 cs207ddl/setup_DB.py
